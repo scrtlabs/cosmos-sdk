5006ca6f6fad27fcaa72f59d19c12274c586a843
Assaf Morami <assaf.morami@gmail.com>
2021-11-17 11:04:22 +0200

    port secretcli tx sign-doc from old version

--- simapp/simd/cmd/root.go
+++ simapp/simd/cmd/root.go
@@ -222,6 +222,7 @@ func txCommand() *cobra.Command {
 	}
 
 	cmd.AddCommand(
+		authcmd.GetSignDocCommand(),
 		authcmd.GetSignCommand(),
 		authcmd.GetSignBatchCommand(),
 		authcmd.GetMultiSignCommand(),

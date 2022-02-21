bdengine/bde-cosmos-sdk

feat: use gRPC for queries
fixes #10996

Signed-off-by: Artur Troian <troian.ap@gmail.com>

client/cmd.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
@@ -8,6 +9,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/tendermint/tendermint/libs/cli"
+	"google.golang.org/grpc"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
@@ -21,7 +23,7 @@ const ClientContextKey = sdk.ContextKey("client.context")
 // SetCmdClientContextHandler is to be used in a command pre-hook execution to
 // read flags that populate a Context and sets that to the command's Context.
 func SetCmdClientContextHandler(clientCtx Context, cmd *cobra.Command) (err error) {
-	clientCtx, err = ReadPersistentCommandFlags(clientCtx, cmd.Flags())
+	clientCtx, err = ReadPersistentCommandFlags(cmd.Context(), clientCtx, cmd.Flags())
 	if err != nil {
 		return err
 	}
@@ -87,7 +89,7 @@ func ValidateCmd(cmd *cobra.Command, args []string) error {
 // - client.Context field not pre-populated & flag set: uses set flag value
 // - client.Context field pre-populated & flag not set: uses pre-populated value
 // - client.Context field pre-populated & flag set: uses set flag value
-func ReadPersistentCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
+func ReadPersistentCommandFlags(ctx context.Context, clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
 	if clientCtx.OutputFormat == "" || flagSet.Changed(cli.OutputFlag) {
 		output, _ := flagSet.GetString(cli.OutputFlag)
 		clientCtx = clientCtx.WithOutputFormat(output)
@@ -133,8 +135,8 @@ func ReadPersistentCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Cont
 		}
 	}
 
+	rpcURI, _ := flagSet.GetString(flags.FlagNode)
 	if clientCtx.Client == nil || flagSet.Changed(flags.FlagNode) {
-		rpcURI, _ := flagSet.GetString(flags.FlagNode)
 		if rpcURI != "" {
 			clientCtx = clientCtx.WithNodeURI(rpcURI)
 
@@ -144,6 +146,17 @@ func ReadPersistentCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Cont
 			}
 
 			clientCtx = clientCtx.WithClient(client)
+		}
+	}
+
+	isOffline, _ := flagSet.GetBool(flags.FlagOffline)
+	if !isOffline && clientCtx.ClientConn == nil && flagSet.Changed(flags.FlagGRPC) {
+		grpcURI, _ := flagSet.GetString(flags.FlagGRPC)
+
+		if grpcURI != "" {
+			if gcl, err := grpc.DialContext(ctx, grpcURI, grpc.WithInsecure(), grpc.WithReturnConnectionError()); err == nil {
+				clientCtx = clientCtx.WithClientConn(gcl)
+			}
 		}
 	}
 
@@ -160,7 +173,7 @@ func ReadPersistentCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Cont
 // - client.Context field not pre-populated & flag set: uses set flag value
 // - client.Context field pre-populated & flag not set: uses pre-populated value
 // - client.Context field pre-populated & flag set: uses set flag value
-func readQueryCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
+func readQueryCommandFlags(ctx context.Context, clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
 	if clientCtx.Height == 0 || flagSet.Changed(flags.FlagHeight) {
 		height, _ := flagSet.GetInt64(flags.FlagHeight)
 		clientCtx = clientCtx.WithHeight(height)
@@ -171,7 +184,7 @@ func readQueryCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, 
 		clientCtx = clientCtx.WithUseLedger(useLedger)
 	}
 
-	return ReadPersistentCommandFlags(clientCtx, flagSet)
+	return ReadPersistentCommandFlags(ctx, clientCtx, flagSet)
 }
 
 // readTxCommandFlags returns an updated Context with fields set based on flags
@@ -184,8 +197,8 @@ func readQueryCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, 
 // - client.Context field not pre-populated & flag set: uses set flag value
 // - client.Context field pre-populated & flag not set: uses pre-populated value
 // - client.Context field pre-populated & flag set: uses set flag value
-func readTxCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
-	clientCtx, err := ReadPersistentCommandFlags(clientCtx, flagSet)
+func readTxCommandFlags(ctx context.Context, clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
+	clientCtx, err := ReadPersistentCommandFlags(ctx, clientCtx, flagSet)
 	if err != nil {
 		return clientCtx, err
 	}
@@ -294,7 +307,7 @@ func readTxCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, err
 // - client.Context field pre-populated & flag set: uses set flag value
 func GetClientQueryContext(cmd *cobra.Command) (Context, error) {
 	ctx := GetClientContextFromCmd(cmd)
-	return readQueryCommandFlags(ctx, cmd.Flags())
+	return readQueryCommandFlags(cmd.Context(), ctx, cmd.Flags())
 }
 
 // GetClientTxContext returns a Context from a command with fields set based on flags
@@ -306,7 +319,7 @@ func GetClientQueryContext(cmd *cobra.Command) (Context, error) {
 // - client.Context field pre-populated & flag set: uses set flag value
 func GetClientTxContext(cmd *cobra.Command) (Context, error) {
 	ctx := GetClientContextFromCmd(cmd)
-	return readTxCommandFlags(ctx, cmd.Flags())
+	return readTxCommandFlags(cmd.Context(), ctx, cmd.Flags())
 }
 
 // GetClientContextFromCmd returns a Context from a command or an empty Context

client/config/config_test.go
@@ -12,7 +12,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/config"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	"github.com/cosmos/cosmos-sdk/x/staking/client/cli"
 )
 
@@ -25,9 +27,12 @@ const (
 // initClientContext initiates client Context for tests
 func initClientContext(t *testing.T, envVar string) (client.Context, func()) {
 	home := t.TempDir()
+	registry := testdata.NewTestInterfaceRegistry()
+
 	clientCtx := client.Context{}.
 		WithHomeDir(home).
-		WithViper("")
+		WithViper("").
+		WithCodec(codec.NewProtoCodec(registry))
 
 	clientCtx.Viper.BindEnv(nodeEnv)
 	if envVar != "" {
@@ -53,7 +58,7 @@ func TestConfigCmd(t *testing.T) {
 	_, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, args)
 	require.NoError(t, err)
 
-	//./build/simd config node //http://localhost:1
+	// ./build/simd config node //http://localhost:1
 	b := bytes.NewBufferString("")
 	cmd.SetOut(b)
 	cmd.SetArgs([]string{"node"})
@@ -68,6 +73,8 @@ func TestConfigCmdEnvFlag(t *testing.T) {
 		defaultNode = "http://localhost:26657"
 	)
 
+	nogrpc := fmt.Sprintf("--%s=", flags.FlagGRPC)
+
 	tt := []struct {
 		name    string
 		envVar  string
@@ -75,9 +82,9 @@ func TestConfigCmdEnvFlag(t *testing.T) {
 		expNode string
 	}{
 		{"env var is set with no flag", testNode1, []string{"validators"}, testNode1},
-		{"env var is set with a flag", testNode1, []string{"validators", fmt.Sprintf("--%s=%s", flags.FlagNode, testNode2)}, testNode2},
-		{"env var is not set with no flag", "", []string{"validators"}, defaultNode},
-		{"env var is not set with a flag", "", []string{"validators", fmt.Sprintf("--%s=%s", flags.FlagNode, testNode2)}, testNode2},
+		{"env var is set with a flag", testNode1, []string{"validators", nogrpc, fmt.Sprintf("--%s=%s", flags.FlagNode, testNode2)}, testNode2},
+		{"env var is not set with no flag", "", []string{"validators", nogrpc}, defaultNode},
+		{"env var is not set with a flag", "", []string{"validators", nogrpc, fmt.Sprintf("--%s=%s", flags.FlagNode, testNode2)}, testNode2},
 	}
 
 	for _, tc := range tt {

scrtlabs/cosmos-sdk

client/cmd.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
@@ -8,6 +9,8 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/tendermint/tendermint/libs/cli"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
@@ -21,7 +24,7 @@ const ClientContextKey = sdk.ContextKey("client.context")
 // SetCmdClientContextHandler is to be used in a command pre-hook execution to
 // read flags that populate a Context and sets that to the command's Context.
 func SetCmdClientContextHandler(clientCtx Context, cmd *cobra.Command) (err error) {
-	clientCtx, err = ReadPersistentCommandFlags(clientCtx, cmd.Flags())
+	clientCtx, err = ReadPersistentCommandFlags(cmd.Context(), clientCtx, cmd.Flags())
 	if err != nil {
 		return err
 	}
@@ -87,7 +90,7 @@ func ValidateCmd(cmd *cobra.Command, args []string) error {
 // - client.Context field not pre-populated & flag set: uses set flag value
 // - client.Context field pre-populated & flag not set: uses pre-populated value
 // - client.Context field pre-populated & flag set: uses set flag value
-func ReadPersistentCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
+func ReadPersistentCommandFlags(ctx context.Context, clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
 	if clientCtx.OutputFormat == "" || flagSet.Changed(cli.OutputFlag) {
 		output, _ := flagSet.GetString(cli.OutputFlag)
 		clientCtx = clientCtx.WithOutputFormat(output)
@@ -133,8 +136,8 @@ func ReadPersistentCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Cont
 		}
 	}
 
+	rpcURI, _ := flagSet.GetString(flags.FlagNode)
 	if clientCtx.Client == nil || flagSet.Changed(flags.FlagNode) {
-		rpcURI, _ := flagSet.GetString(flags.FlagNode)
 		if rpcURI != "" {
 			clientCtx = clientCtx.WithNodeURI(rpcURI)
 
@@ -144,6 +147,23 @@ func ReadPersistentCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Cont
 			}
 
 			clientCtx = clientCtx.WithClient(client)
+		}
+	}
+
+	if clientCtx.ClientConn == nil || flagSet.Changed(flags.FlagGRPCNode) {
+		grpcURI, _ := flagSet.GetString(flags.FlagGRPCNode)
+
+		if grpcURI != "" {
+			grpcKeepAlive, _ := flagSet.GetDuration(flags.FlagGRPCKeepAlive)
+			gcl, err := grpc.DialContext(ctx, grpcURI,
+				grpc.WithInsecure(),
+				grpc.WithKeepaliveParams(keepalive.ClientParameters{
+					Time: grpcKeepAlive,
+				}),
+			)
+			if err == nil {
+				clientCtx = clientCtx.WithClientConn(gcl)
+			}
 		}
 	}
 
@@ -160,7 +180,7 @@ func ReadPersistentCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Cont
 // - client.Context field not pre-populated & flag set: uses set flag value
 // - client.Context field pre-populated & flag not set: uses pre-populated value
 // - client.Context field pre-populated & flag set: uses set flag value
-func readQueryCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
+func readQueryCommandFlags(ctx context.Context, clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
 	if clientCtx.Height == 0 || flagSet.Changed(flags.FlagHeight) {
 		height, _ := flagSet.GetInt64(flags.FlagHeight)
 		clientCtx = clientCtx.WithHeight(height)
@@ -171,7 +191,7 @@ func readQueryCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, 
 		clientCtx = clientCtx.WithUseLedger(useLedger)
 	}
 
-	return ReadPersistentCommandFlags(clientCtx, flagSet)
+	return ReadPersistentCommandFlags(ctx, clientCtx, flagSet)
 }
 
 // readTxCommandFlags returns an updated Context with fields set based on flags
@@ -184,8 +204,8 @@ func readQueryCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, 
 // - client.Context field not pre-populated & flag set: uses set flag value
 // - client.Context field pre-populated & flag not set: uses pre-populated value
 // - client.Context field pre-populated & flag set: uses set flag value
-func readTxCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
-	clientCtx, err := ReadPersistentCommandFlags(clientCtx, flagSet)
+func readTxCommandFlags(ctx context.Context, clientCtx Context, flagSet *pflag.FlagSet) (Context, error) {
+	clientCtx, err := ReadPersistentCommandFlags(ctx, clientCtx, flagSet)
 	if err != nil {
 		return clientCtx, err
 	}
@@ -263,7 +283,7 @@ func readTxCommandFlags(clientCtx Context, flagSet *pflag.FlagSet) (Context, err
 // - client.Context field pre-populated & flag set: uses set flag value
 func GetClientQueryContext(cmd *cobra.Command) (Context, error) {
 	ctx := GetClientContextFromCmd(cmd)
-	return readQueryCommandFlags(ctx, cmd.Flags())
+	return readQueryCommandFlags(cmd.Context(), ctx, cmd.Flags())
 }
 
 // GetClientTxContext returns a Context from a command with fields set based on flags
@@ -275,7 +295,7 @@ func GetClientQueryContext(cmd *cobra.Command) (Context, error) {
 // - client.Context field pre-populated & flag set: uses set flag value
 func GetClientTxContext(cmd *cobra.Command) (Context, error) {
 	ctx := GetClientContextFromCmd(cmd)
-	return readTxCommandFlags(ctx, cmd.Flags())
+	return readTxCommandFlags(cmd.Context(), ctx, cmd.Flags())
 }
 
 // GetClientContextFromCmd returns a Context from a command or an empty Context

client/config/config_test.go
@@ -12,7 +12,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/config"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	"github.com/cosmos/cosmos-sdk/x/staking/client/cli"
 )
 
@@ -25,9 +27,12 @@ const (
 // initClientContext initiates client Context for tests
 func initClientContext(t *testing.T, envVar string) (client.Context, func()) {
 	home := t.TempDir()
+	registry := testdata.NewTestInterfaceRegistry()
+
 	clientCtx := client.Context{}.
 		WithHomeDir(home).
-		WithViper("")
+		WithViper("").
+		WithCodec(codec.NewProtoCodec(registry))
 
 	clientCtx.Viper.BindEnv(nodeEnv)
 	if envVar != "" {
@@ -53,7 +58,7 @@ func TestConfigCmd(t *testing.T) {
 	_, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, args)
 	require.NoError(t, err)
 
-	//./build/simd config node //http://localhost:1
+	// ./build/simd config node //http://localhost:1
 	b := bytes.NewBufferString("")
 	cmd.SetOut(b)
 	cmd.SetArgs([]string{"node"})
@@ -68,6 +73,8 @@ func TestConfigCmdEnvFlag(t *testing.T) {
 		defaultNode = "http://localhost:26657"
 	)
 
+	nogrpc := fmt.Sprintf("--%s=", flags.FlagGRPCNode)
+
 	tt := []struct {
 		name    string
 		envVar  string
@@ -75,9 +82,9 @@ func TestConfigCmdEnvFlag(t *testing.T) {
 		expNode string
 	}{
 		{"env var is set with no flag", testNode1, []string{"validators"}, testNode1},
-		{"env var is set with a flag", testNode1, []string{"validators", fmt.Sprintf("--%s=%s", flags.FlagNode, testNode2)}, testNode2},
-		{"env var is not set with no flag", "", []string{"validators"}, defaultNode},
-		{"env var is not set with a flag", "", []string{"validators", fmt.Sprintf("--%s=%s", flags.FlagNode, testNode2)}, testNode2},
+		{"env var is set with a flag", testNode1, []string{"validators", nogrpc, fmt.Sprintf("--%s=%s", flags.FlagNode, testNode2)}, testNode2},
+		{"env var is not set with no flag", "", []string{"validators", nogrpc}, defaultNode},
+		{"env var is not set with a flag", "", []string{"validators", nogrpc, fmt.Sprintf("--%s=%s", flags.FlagNode, testNode2)}, testNode2},
 	}
 
 	for _, tc := range tt {
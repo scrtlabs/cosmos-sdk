7f7a6dddf56462bea8927a906e58c0d4813dc1c9
HuangYi <huang@crypto.com>
2022-03-11 09:26:24 +0200

    Fix tendermint rollback in 1.6?

--- server/rollback.go
+++ server/rollback.go
@@ -1,13 +1,20 @@
 package server
 
 import (
+	"errors"
 	"fmt"
+	cfg "github.com/tendermint/tendermint/config"
+	"github.com/tendermint/tendermint/state"
+	"github.com/tendermint/tendermint/store"
+	dbm "github.com/tendermint/tm-db"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/server/types"
 	"github.com/spf13/cobra"
 	tmcmd "github.com/tendermint/tendermint/cmd/tendermint/commands"
 )
+
+const FlagDeletePendingBlock = "delete-pending-block"
 
 // NewRollbackCmd creates a command to rollback tendermint and multistore state by one height.
 func NewRollbackCmd(appCreator types.AppCreator, defaultNodeHome string) *cobra.Command {
@@ -25,6 +32,18 @@ application.
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := GetServerContextFromCmd(cmd)
 			cfg := ctx.Config
+
+			deletePendingBlock, err := cmd.Flags().GetBool(FlagDeletePendingBlock)
+			if err != nil {
+				return err
+			}
+
+			if deletePendingBlock {
+				if err := deletePendingBlockIfExists(cfg); err != nil {
+					return err
+				}
+			}
+
 			home := cfg.RootDir
 			db, err := openDB(home)
 			if err != nil {
@@ -48,5 +67,54 @@ application.
 	}
 
 	cmd.Flags().String(flags.FlagHome, defaultNodeHome, "The application home directory")
+	cmd.Flags().Bool(FlagDeletePendingBlock, false, "Delete the pending block in tendermint block store if exists")
 	return cmd
+}
+
+func loadStateAndBlockStore(config *cfg.Config) (*store.BlockStore, state.Store, error) {
+	dbType := dbm.BackendType(config.DBBackend)
+
+	// Get BlockStore
+	blockStoreDB, err := dbm.NewDB("blockstore", dbType, config.DBDir())
+	if err != nil {
+		return nil, nil, err
+	}
+	blockStore := store.NewBlockStore(blockStoreDB)
+
+	// Get StateStore
+	stateDB, err := dbm.NewDB("state", dbType, config.DBDir())
+	if err != nil {
+		return nil, nil, err
+	}
+	stateStore := state.NewStore(stateDB, state.StoreOptions{DiscardABCIResponses: false})
+
+	return blockStore, stateStore, nil
+}
+
+func deletePendingBlockIfExists(config *cfg.Config) error {
+	blockStore, stateStore, err := loadStateAndBlockStore(config)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = blockStore.Close()
+		_ = stateStore.Close()
+	}()
+	tmState, err := stateStore.Load()
+	if err != nil {
+		return err
+	}
+	if tmState.IsEmpty() {
+		return errors.New("no state found")
+	}
+
+	height := blockStore.Height()
+	if height == tmState.LastBlockHeight+1 {
+		// delete this pending block
+		if err := blockStore.Rollback(); err != nil {
+			return err
+		}
+		fmt.Println("rollback pending block", height)
+	}
+	return nil
 }

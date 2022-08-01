841be4329b9945f9e1fde1a2864d4d35e14ccaf4
Xiangan He <xiangan9@gmail.com>
2022-08-01 16:04:45 -0400

    added value & migrated params

--- x/gov/legacy/v4/export_test.go
+++ x/gov/legacy/v4/export_test.go
@@ -1,3 +1,6 @@
 package v4
 
 var MinInitialDepositRatio = minInitialDepositRatio
+var MinExpeditedDeposit = minExpeditedDeposit
+var ExpeditedVotingPeriod = expeditedVotingPeriod
+var ExpeditedThreshold = expeditedThreshold
--- x/gov/legacy/v4/store.go
+++ x/gov/legacy/v4/store.go
@@ -1,11 +1,19 @@
 package v4
 
 import (
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
+// If expedited, the deposit to enter voting period will be
+// increased to 5000 OSMO. The proposal will have 24 hours to achieve
+// a two-thirds majority of all staked OSMO voting power voting YES.
 var minInitialDepositRatio = sdk.NewDec(25).Quo(sdk.NewDec(100))
+var minExpeditedDeposit = sdk.NewCoins(sdk.NewCoin("osmo", sdk.NewInt(5000)))
+var expeditedVotingPeriod = time.Duration(time.Hour * 24)
+var expeditedThreshold = sdk.NewDec(2).Quo(sdk.NewDec(3))
 
 // MigrateStore performs in-place store migrations for consensus version 3
 // in the gov module.
@@ -22,7 +30,22 @@ func MigrateStore(ctx sdk.Context, paramstore types.ParamSubspace) error {
 
 func migrateParamsStore(ctx sdk.Context, paramstore types.ParamSubspace) {
 	var depositParams types.DepositParams
+	var votingParams types.VotingParams
+	var tallyParams types.TallyParams
+
+	//Set depositParams
 	paramstore.Get(ctx, types.ParamStoreKeyDepositParams, &depositParams)
 	depositParams.MinInitialDepositRatio = minInitialDepositRatio
+	depositParams.MinExpeditedDeposit = minExpeditedDeposit
 	paramstore.Set(ctx, types.ParamStoreKeyDepositParams, depositParams)
+
+	//Set votingParams
+	paramstore.Get(ctx, types.ParamStoreKeyDepositParams, &votingParams)
+	votingParams.ExpeditedVotingPeriod = expeditedVotingPeriod
+	paramstore.Set(ctx, types.ParamStoreKeyDepositParams, votingParams)
+
+	//Set tallyParams
+	paramstore.Get(ctx, types.ParamStoreKeyDepositParams, &votingParams)
+	tallyParams.ExpeditedThreshold = expeditedThreshold
+	paramstore.Set(ctx, types.ParamStoreKeyDepositParams, votingParams)
 }

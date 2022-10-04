c191a809ad5fd4fbff60a17f524e86c3e2698720
Adam Tucker <adam@osmosis.team>
2022-10-04 12:48:59 -0500

    Update genesis.go (#340)

--- x/auth/types/genesis.go
+++ x/auth/types/genesis.go
@@ -33,6 +33,9 @@ func NewGenesisState(params Params, accounts GenesisAccounts) *GenesisState {
 func (g GenesisState) UnpackInterfaces(unpacker types.AnyUnpacker) error {
 	for _, any := range g.Accounts {
 		var account GenesisAccount
+		if any.TypeUrl == "/ibc.applications.interchain_accounts.v1.InterchainAccount" {
+			continue
+		}
 		err := unpacker.UnpackAny(any, &account)
 		if err != nil {
 			return err

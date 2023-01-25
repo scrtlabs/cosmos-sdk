b4ff21df288626f89c5c11d611dc7065d555299e
Itzik Grossman <itzik@scrtlabs.com>
2023-01-25 11:59:16 +0200

    Fix many tests

--- x/staking/client/testutil/test_helpers.go
+++ x/staking/client/testutil/test_helpers.go
@@ -15,10 +15,7 @@ import (
 var commonArgs = []string{
 	fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
 	fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
-	fmt.Sprintf("--%s=%s", flags.FlagGasPrices,
-		sdk.NewDecCoins(sdk.NewDecCoinFromDec(sdk.DefaultBondDenom, sdk.NewDecWithPrec(4000000000000000, sdk.Precision))).String()),
-
-	// fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(10))).String()),
+	fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(10))).String()),
 }
 
 // MsgRedelegateExec creates a redelegate message.

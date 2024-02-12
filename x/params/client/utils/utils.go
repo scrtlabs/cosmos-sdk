a0e0d5ef0b052dc8c618be9600615ec9e6e6ec42
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2024-02-12 11:13:24 +0300

    Merge params except REST

--- x/params/client/utils/utils.go
+++ x/params/client/utils/utils.go
@@ -26,6 +26,7 @@ type (
 	ParamChangeProposalJSON struct {
 		Title       string           `json:"title" yaml:"title"`
 		Description string           `json:"description" yaml:"description"`
+		IsExpedited bool             `json:"is_expedited" yaml:"is_expedited"`
 		Changes     ParamChangesJSON `json:"changes" yaml:"changes"`
 		Deposit     string           `json:"deposit" yaml:"deposit"`
 	}
